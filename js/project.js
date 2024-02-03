497e00e6e5511b0a930778dc882da2d0b7ab68af
mahsa <mahsa.karamooz@gmail.com>
2024-02-03 14:42:18 +0330

    first commit

---
+++ js/project.js
@@ -0,0 +1,6 @@
+let caret = document.getElementById('caret')
+caret.addEventListener('click' , ()=>{
+    caret.lastElementChild.classList.toggle('fa-angle-down')
+    caret.lastElementChild.classList.toggle('fa-angle-up')
+
+})
